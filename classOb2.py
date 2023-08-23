efded82de02e184b1197e073c1477e94afd9f4b0
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-08-23 23:52:39 +0530

    initial.commit
    IT IS A PYTHON CODE TO IMPLEMENT CLASS AND OBJECT

---
+++ classOb2.py
@@ -0,0 +1,10 @@
+class DemoClass:
+    a=10
+    
+    def sumvalue(self):
+        print(self.a)
+        
+demoobject = DemoClass()
+
+
+demoobject.sumvalue();
---
+++ classObject.py
@@ -0,0 +1,12 @@
+class DemoClass:
+    a=10
+    
+    def sumvalue(self):
+        print(20+20)
+        
+demoobject = DemoClass()
+demoobject1 = DemoClass()
+print(demoobject.a)
+print(demoobject1.a)
+
+demoobject.sumvalue();
